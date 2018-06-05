fe045e14187b0c0db3e129ee640277c6d0b7cb27
Santiago Garza <santiago.garza@imforza.com>
2018-06-04 21:16:11 -0700

    Expand lib

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ### Pi Bot Arduino
 
-- Set up to use [platformIO](https://platformio.org/) instead of default arduino IDE. (Needs testing).
+- Set up to use [platformIO](https://platformio.org/) instead of default arduino IDE.
 
-- Uses prebuilt arduino library for motor controller. https://github.com/AndreaLombardo/L298N
+- Uses prebuilt arduino library for motor controller. https://github.com/AndreaLombardo/L298N.
+
+#### Getting Started
+- Move lib/PinMap/src/PinMap.h.sample to lib/PinMap/src/PinMap.h
+- Configure pin numbers in PinMap.h to correspond with the wiring for your robot.
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,23 @@
 #include <Arduino.h>
+#include <Helpers.h>
 #include <L298N.h>
+#include <PinMap.h>
+
+#define JOYSTICK_MIN 0.0
+#define JOYSTICK_MAX 1.0
+#define PWM_MIN 0.0
+#define PWM_MAX 255.0
 
 const long BAUD_RATE = 250000;
 
-#define ledPin 13
-
-// motor one
-#define ENA 10
-#define IN1 9
-#define IN2 8
-// motor two
-#define ENB 5
-#define IN3 7
-#define IN4 6
-
-L298N frontLeft(ENA, IN1, IN2);
+L298N backRight(ENA, IN1, IN2);
 L298N frontRight(ENB, IN3, IN4);
+L298N frontLeft(ENC, IN5, IN4);
+L298N backLeft(END, IN7, IN8);
 
 float  joystick_val = 0; 
-float    pwmOutput = 255;
+float  pwmOutput    = PWM_MIN;
 char   command[20];
-
-float mapf(float x, float in_min, float in_max, float out_min, float out_max)
-{
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
 
 void setup() {
 
@@ -36,16 +29,23 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB
   }
   
+  frontLeft.setSpeed( PWM_MIN );
+  backLeft.setSpeed( PWM_MIN );
+  frontRight.setSpeed( PWM_MIN );
+  backRight.setSpeed( PWM_MIN );
+  
 }
 
 void loop() {
   
   // Read data only when available.
-  if (Serial.available() > 0) {
-      
+  /*if (Serial.available() > 0) {
+   
+    Serial.println( 'BAR' );
+    
     Serial.readBytesUntil(':',command, 8); // First read command.
     joystick_val = Serial.parseFloat(); // Then the command value.
-    pwmOutput = mapf(joystick_val, 0.0, 1.0, 0.0, 255.0); // Convert to PWM value.
+    pwmOutput = mapf(joystick_val, JOYSTICK_MIN, JOYSTICK_MAX, PWM_MIN, PWM_MAX); // Convert to PWM value.
     
     Serial.print( command );
     Serial.print( ":" );
@@ -58,6 +58,6 @@ void loop() {
     frontLeft.setSpeed( pwmOutput );
    }else if(strcmp(command, "x2")  == 0){
     frontRight.setSpeed( pwmOutput );
-   }
+  }*/
 
 }
